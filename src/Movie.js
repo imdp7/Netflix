c59327689089f866e7c41fce8587649102ab3a93
Darshan Patel <patel7darshan97@gmail.com>
2022-03-04 04:45:35 -0500

    more vidoes

--- src/Movie.js
+++ src/Movie.js
@@ -9,6 +9,7 @@ import { Link } from 'react-router-dom';
 import Modal from "@material-tailwind/react/Modal";
 import ModalHeader from "@material-tailwind/react/ModalHeader";
 import ModalBody from "@material-tailwind/react/ModalBody";
+import WatchProvider from './WatchProvider';
 
 
 function truncate(str,n){
@@ -18,9 +19,11 @@ function truncate(str,n){
 function Movie({match}) {
     const base_url="https://image.tmdb.org/t/p/original/"
     const [movie,setMovie] = useState([])
+    const [trailer,setTrailer] = useState([]);
     const [trailerUrl, setTrailerUrl]= useState("");
     const [recommends, setRecommends]= useState([]); 
     const [credits, setCredits]= useState([]); 
+    const [providers, setProviders]= useState([]); 
     const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
@@ -31,6 +34,15 @@ function Movie({match}) {
         async function fetchData() {
             const request = await axios.get(`https://api.themoviedb.org/3/movie/${match.params.id}?api_key=${API_KEY}`);
             setMovie(request.data);
+            return request;
+        }
+        fetchData();
+    },[match.params.id,API_KEY]);
+
+      useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(`https://api.themoviedb.org/3/movie/${match.params.id}/videos?api_key=${API_KEY}&language=en-US`);
+            setTrailer(request.data.results);
             return request;
         }
         fetchData();
@@ -54,13 +66,19 @@ function Movie({match}) {
           fetchData();
         },[match.params.id,API_KEY]);
 
-    const opts = {
-      height: '390',
-      width: '840',
-        playerVars: {
-            autoplay: 1,
+      useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(`https://api.themoviedb.org/3/movie/${match.params.id}/watch/providers?api_key=${API_KEY}`);
+            setProviders(request.data.results);
+            return request;
+          }
+          fetchData();
+        },[match.params.id,API_KEY]);
 
-        },
+
+    const opts = {
+      height: 390,
+      width: 840,
     };
 
     const handleClick = (movie) => {
@@ -115,10 +133,11 @@ function Movie({match}) {
                 <h1 className="banner__title">
                  {movie?.title || movie?.name || movie?.original_name}
                 </h1>
-                <div className="flex">
+                <div className="flex span-x-8">
                 <button className="banner__button" onClick={() => handleClick(movie)}>Play</button>
                 <button className="banner__button">My List</button> 
-                <button className={`text-white py-3 text-lg font-bold font-sans rounded-lg px-4 hover:bg-gray-300 hover:text-black ${movie?.vote_average < 5 ? "bg-red-500" : "bg-green-500"}`}>{movie?.vote_average} stars</button> 
+
+                <button className={`text-white py-3 text-lg font-bold font-sans rounded-lg px-4 hover:bg-gray-300 hover:text-black ${movie?.vote_average < 7 ? "bg-red-500" : "bg-green-500"}`}>{movie?.vote_average} stars</button> 
                 </div>
                 <h1 className="banner__description">{truncate(movie?.overview,350)}</h1>
              </div>
@@ -131,59 +150,69 @@ function Movie({match}) {
             </div> 
              <div className='flex flex-row p-2 pb-4 bg-black flex-wrap'>
                 <div className='flex flex-col itmes-center text-white m-2 max-w-4xl w-2xl'>
+             {movie?.release_date &&(
                 <div className='p-2 flex flex-row'>
                   <span className='font-bold text-lg'>
                     Release Date : &nbsp;
                   </span>
-             {movie.release_date &&(
                 <span className='font-semibold text-lg'>
                 {' '}{movie.release_date}
                 </span>
-                )}
                 </div>
+                )}
+             {movie?.belongs_to_collection == "null" &&(
+                <div className='p-2 flex flex-row'>
+                  <span className='font-bold text-lg'>
+                  Belongs to Collection : &nbsp;
+                  </span>
+                <span className='font-semibold text-lg'>
+                {' '}{movie?.belongs_to_collection}
+                </span>
+                </div>
+                )}
+             {movie.genres &&(
                 <div className='p-2 flex flex-row'>
                   <span className='font-bold text-lg'>
                     Genre : &nbsp;
                   </span>
-             {movie.genres &&(
                 <span className='font-semibold text-lg'>
                 {' '}{movie.genres[0].name}
                 </span>
-                )}
                 </div>
+                )}
+                {movie.vote_count && (
                 <div className='p-2 flex flex-row '>
                 <span className='font-bold text-lg'>
                     Vote : &nbsp;
                   </span>
-                {movie.vote_count && (
                 <span className='font-semibold text-lg'>{movie.vote_count} votes</span>
-                )}
                 </div>
+                )}
+                {movie.status && (
                 <div className='p-2 flex flex-row'>
                 <span className='font-bold text-lg'>
                     Status : &nbsp;
                   </span>
-                {movie.status && (
                 <span className={`font-semibold text-lg ${movie.status =="Released" ? "text-green-500" : "text-red-500"}`}>{movie.status}</span> 
-                )}
                 </div>
+                )}
+                {movie?.budget && (
                 <div className='p-2 flex flex-row'>
                 <span className='font-bold text-lg'>
                     Budget : &nbsp;
                   </span>
-                {movie.budget && (
-                <span className="font-semibold text-lg"><CurrencyFormat value={movie.budget} displayType={'text'} thousandSeparator={true} prefix={'$'} renderText={value => <div>{value}</div>} />
+                <span className="font-semibold text-lg"><CurrencyFormat value={movie?.budget} displayType={'text'} thousandSeparator={true} prefix={'$'} renderText={value => <div>{value}</div>} />
                 </span> 
-                )}
                 </div>
+                )}
+                {movie.overview && (
                 <div className='p-2 flex flex-col'>
                 <span className='font-bold text-lg'>
                     Overview : &nbsp;
                   </span>
-                {movie.overview && (
                 <span className="font-semibold text-lg">{movie.overview}</span> 
-                )}
                 </div>
+                )}
                 {movie?.production_countries && (
                 <div>
                 <div className='p-2 flex flex-row mb-2'>
@@ -227,6 +256,10 @@ function Movie({match}) {
                 ))}
                 </div>
                 )}
+               
+
+                <WatchProvider provider={providers}/>
+
                 <div class="py-4">
                   <div class="w-full border-t border-gray-300"></div>
                 </div>
@@ -257,6 +290,23 @@ function Movie({match}) {
                 </div>
                 </div>
                 )}
+                  <div class="py-4">
+                <div class="w-full border-t border-gray-300"></div>
+              </div>
+              {trailer && (
+                <div className='w-full'>
+                  <span className='font-extrabold text-white text-2xl'>More Videos</span>
+                  <div className='py-4'>
+                    <div className="grid grid-cols-3 gap-3">
+                  {trailer.map(t => (
+                    <div className='py-2 w-full'>
+                       <YouTube videoId={t.key} className='w-auto h-auto'/>
+                       </div>
+                       ))}
+                       </div>
+                  </div>
+                  </div>
+              )}
               <div class="py-4">
                 <div class="w-full border-t border-gray-300"></div>
               </div>
---
+++ src/WatchProvider.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react'
+
+function WatchProvider({provider}) {
+    console.log(provider)
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default WatchProvider
